12989621ada58e36f08d09abf3d6c8618e9521d9
rahzex <zexprorah@gmail.com>
2025-01-15 01:12:46 +0530

    update

---
+++ src/main/java/org/rahzex/observer/entity/Settings.java
@@ -0,0 +1,11 @@
+package org.rahzex.observer.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Settings {
+    private FontSettings fontSettings;
+    private ThemeSettings themeSettings;
+}
---
+++ src/main/java/org/rahzex/observer/test/TestSettingsUpdater.java
@@ -0,0 +1,48 @@
+package org.rahzex.observer.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.rahzex.observer.entity.FontSettings;
+import org.rahzex.observer.entity.ThemeSettings;
+import org.rahzex.observer.event.SettingsEvent;
+import org.rahzex.observer.observable.SettingUpdatePublisher;
+import org.rahzex.observer.observers.Header;
+import org.rahzex.observer.observers.TextPanel;
+import org.rahzex.observer.service.FontService;
+import org.rahzex.observer.service.ThemeService;
+
+public class TestSettingsUpdater {
+    private FontService fontService;
+    private ThemeService themeService;
+    private SettingUpdatePublisher updatePublisher;
+
+    @BeforeEach
+    void init() {
+        fontService = new FontService();
+        themeService = new ThemeService();
+        updatePublisher = new SettingUpdatePublisher();
+    }
+
+    @Test
+    void testSettingsUpdate() {
+        Header header = new Header();
+        TextPanel textPanel = new TextPanel();
+
+        updatePublisher.subscribe(SettingsEvent.THEME_UPDATED, header);
+        updatePublisher.subscribe(SettingsEvent.THEME_UPDATED, textPanel);
+        updatePublisher.subscribe(SettingsEvent.FONT_UPDATED, textPanel);
+
+        themeService.update(ThemeSettings.builder().primaryColor("Red").accentColor("Maroon").build());
+        fontService.update(FontSettings.builder().fontSize(20).fontStyle("ST11").build());
+
+        updatePublisher.unsubscribe(SettingsEvent.THEME_UPDATED, header);
+        themeService.update(ThemeSettings.builder().primaryColor("White").accentColor("Cream").build());
+
+        Assertions.assertEquals(20, fontService.getFontSettings().getFontSize());
+        Assertions.assertEquals("ST11", fontService.getFontSettings().getFontStyle());
+
+        Assertions.assertEquals("White", themeService.getThemeSettings().getPrimaryColor());
+        Assertions.assertEquals("Cream", themeService.getThemeSettings().getAccentColor());
+    }
+}
