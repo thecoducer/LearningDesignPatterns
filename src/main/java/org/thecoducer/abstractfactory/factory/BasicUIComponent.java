69f78a016bf7d619b13cf0be80a92f5cd35bc5aa
Mayukh Datta <mayukh5741@gmail.com>
2025-01-16 22:00:28 +0530

    redesign GUIFactory

--- src/main/java/org/thecoducer/abstractfactory/factory/BasicUIComponent.java
+++ src/main/java/org/thecoducer/abstractfactory/factory/BasicUIComponent.java
@@ -7,22 +7,12 @@ import org.thecoducer.abstractfactory.uicomponent.checkbox.Checkbox;
 
 @Slf4j
 public class BasicUIComponent {
-  private static GUIFactory guiFactory;
-
-  private static GUIFactory initializeGuiFactory(UIStyle uiStyle) {
-    return switch (uiStyle) {
-      case MAC_OS -> new MacFactory();
-      case WINDOWS_OS -> new WinFactory();
-    };
-  }
 
   public static Button createButton(UIStyle uiStyle) {
-    guiFactory = initializeGuiFactory(uiStyle);
-    return guiFactory.createButton();
+    return GUIFactory.getFactory(uiStyle).createButton();
   }
 
   public static Checkbox createCheckbox(UIStyle uiStyle) {
-    guiFactory = initializeGuiFactory(uiStyle);
-    return guiFactory.createCheckbox();
+    return GUIFactory.getFactory(uiStyle).createCheckbox();
   }
 }
--- src/main/java/org/thecoducer/abstractfactory/factory/GUIFactory.java
+++ src/main/java/org/thecoducer/abstractfactory/factory/GUIFactory.java
@@ -1,9 +1,18 @@
 package org.thecoducer.abstractfactory.factory;
 
+import org.thecoducer.abstractfactory.enums.UIStyle;
 import org.thecoducer.abstractfactory.uicomponent.button.Button;
 import org.thecoducer.abstractfactory.uicomponent.checkbox.Checkbox;
 
 public interface GUIFactory {
   Button createButton();
+
   Checkbox createCheckbox();
+
+  static GUIFactory getFactory(UIStyle uiStyle) {
+    return switch (uiStyle) {
+      case MAC_OS -> new MacFactory();
+      case WINDOWS_OS -> new WinFactory();
+    };
+  }
 }
