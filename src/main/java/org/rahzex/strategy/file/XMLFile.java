045540e649dead2cfdac0bfc950661bc13512eec
rahzex <zexprorah@gmail.com>
2025-01-18 00:52:42 +0530

    Implemented Strategy with Factory Pattern

---
+++ src/main/java/org/rahzex/strategy/file/XMLFile.java
@@ -0,0 +1,7 @@
+package org.rahzex.strategy.file;
+
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder(toBuilder = true)
+public class XMLFile extends File {
+}
---
+++ src/main/java/org/rahzex/strategy/uploader/PDFFileUploader.java
@@ -0,0 +1,13 @@
+package org.rahzex.strategy.uploader;
+
+import lombok.extern.slf4j.Slf4j;
+import org.rahzex.strategy.file.File;
+
+@Slf4j
+public class PDFFileUploader extends FileUploader{
+
+    @Override
+    public void upload(File file) {
+        super.upload(file);
+    }
+}
