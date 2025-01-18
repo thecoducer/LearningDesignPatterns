efa3a345614caf0f42e3e5199035660cdf919892
Mayukh Datta <mayukh5741@gmail.com>
2025-01-18 16:29:33 +0530

    redesigned

--- src/main/java/org/thecoducer/adapter/JsonXmlConverter.java
+++ src/main/java/org/thecoducer/adapter/JsonXmlConverter.java
@@ -1,5 +1,8 @@
 package org.thecoducer.adapter;
 
+/**
+ * The JsonXmlConverter class is an adapter responsible for converting JSON files to XML files and vice versa.
+ */
 public class JsonXmlConverter {
   public static XMLFile doForward(JSONFile input) {
     return new XMLFile(input.getData().split(".json")[0] + ".xml");
--- src/main/java/org/thecoducer/adapter/MetabaseService.java
+++ src/main/java/org/thecoducer/adapter/MetabaseService.java
@@ -2,13 +2,14 @@ package org.thecoducer.adapter;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * The MetabaseService is responsible for retrieving data from the Metabase.
+ * The data comes in JSON format.
+ */
 @Slf4j
 public class MetabaseService {
-  private StockDataProvider stockDataProvider = new StockDataProvider();
-
-  public void sendData(JSONFile jsonFile) {
-    XMLFile xmlFile = JsonXmlConverter.doForward(jsonFile);
-    log.info("Sending XMLFile to StockDataProvider");
-    stockDataProvider.sendData(xmlFile);
+  public JSONFile getData() {
+    // gets the real data from the Metabase
+    return new JSONFile("Sales.json");
   }
 }
