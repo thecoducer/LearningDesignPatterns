da2fc7bd2e8ead57b55a634d29e0759a8e767cdf
thecoducer <24296902+thecoducer@users.noreply.github.com>
2025-01-18 21:31:58 +0000

    Apply formatting changes

--- src/main/java/org/rahzex/decorator/TextDecorationManager.java
+++ src/main/java/org/rahzex/decorator/TextDecorationManager.java
@@ -4,12 +4,9 @@ public class TextDecorationManager {
   public String decorate(TextDecorationPreferences preferences) {
     Text text = new PlainText();
 
-    if (preferences.isMakeBold())
-      text = new BoldText(text);
-    if (preferences.isMakeItalic())
-      text = new ItalicText(text);
-    if (preferences.isMakeUnderlined())
-      text = new UnderlinedText(text);
+    if (preferences.isMakeBold()) text = new BoldText(text);
+    if (preferences.isMakeItalic()) text = new ItalicText(text);
+    if (preferences.isMakeUnderlined()) text = new UnderlinedText(text);
 
     return text.process();
   }
