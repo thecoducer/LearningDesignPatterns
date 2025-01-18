5b66791c28d396190bd9ab85e8b7ca5c9b1e8668
Mayukh Datta <mayukh5741@gmail.com>
2025-01-18 16:29:33 +0530

    redesigned

--- src/main/java/org/thecoducer/adapter/AnalyticsService.java
+++ src/main/java/org/thecoducer/adapter/AnalyticsService.java
@@ -2,6 +2,10 @@ package org.thecoducer.adapter;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * The AnalyticsService performs analysis on the data that comes from StockDataProcessor.
+ * AnalyticsService can handle only JSON files.
+ */
 @Slf4j
 public class AnalyticsService {
   public void doAnalysis(JSONFile jsonFile) {
