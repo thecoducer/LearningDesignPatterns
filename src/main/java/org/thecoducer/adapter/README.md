205abbd72fe19669ca86652a0c02bd9fcc087391
Mayukh Datta <mayukh5741@gmail.com>
2025-01-18 16:29:33 +0530

    redesigned

---
+++ src/main/java/org/thecoducer/adapter/README.md
@@ -0,0 +1,4 @@
+* StockDataProcessor is a service that processes the stock data and returns the processed data. It can work with XML files only.
+* We fetch the real data from the MetabaseService that gives us the data in the form of JSON.
+* AnalyticsService performs analysis on the processed stock data. It can work with JSON only.
+* We use JsonXmlConverter to convert the JSON data to XML data and vice versa and is used as an adapter between services to overcome data incompatibility.
--- src/test/java/org/thecoducer/adapter/ApplicationTest.java
+++ src/test/java/org/thecoducer/adapter/ApplicationTest.java
@@ -7,32 +7,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest {
-  private StockDataProvider stockDataProvider;
+  private StockDataProcessor stockDataProcessor;
   private MetabaseService metabaseService;
+  private AnalyticsService analyticsService;
   private LogCaptor analyticsServiceLogCaptor;
-  private LogCaptor metabaseServiceLogCaptor;
 
   @BeforeEach
   public void setUp() {
-    stockDataProvider = new StockDataProvider();
-    analyticsServiceLogCaptor = LogCaptor.forClass(AnalyticsService.class);
+    stockDataProcessor = new StockDataProcessor();
     metabaseService = new MetabaseService();
-    metabaseServiceLogCaptor = LogCaptor.forClass(MetabaseService.class);
+    analyticsService = new AnalyticsService();
+    analyticsServiceLogCaptor = LogCaptor.forClass(AnalyticsService.class);
   }
 
   @Test
-  public void testSendDataFromStockDataProvider() {
-    XMLFile xmlFile = new XMLFile("January/Sales.xml");
-    stockDataProvider.sendData(xmlFile);
-    assertEquals("Analysing... January/Sales.json", analyticsServiceLogCaptor.getInfoLogs().getFirst());
-  }
-
-  @Test
-  public void testSendDataFromMetabaseService() {
-    JSONFile jsonFile = new JSONFile("January/Sales.json");
-    metabaseService.sendData(jsonFile);
-    assertEquals("Sending XMLFile to StockDataProvider", metabaseServiceLogCaptor.getInfoLogs().getFirst());
-    assertEquals("Analysing... January/Sales.json", analyticsServiceLogCaptor.getInfoLogs().getFirst());
+  public void testDataAnalysis() {
+    JSONFile jsonFile = metabaseService.getData();
+    XMLFile xmlFile = JsonXmlConverter.doForward(jsonFile);
+    XMLFile processedXMLFile = stockDataProcessor.processData(xmlFile);
+    analyticsService.doAnalysis(JsonXmlConverter.doBackward(processedXMLFile));
+    assertEquals("Analysing... Sales_processed.json", analyticsServiceLogCaptor.getInfoLogs().getFirst());
   }
 
 }
